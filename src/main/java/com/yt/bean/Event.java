2b3e8f1b07e43ea02d82076e2bd9c96786378691
potato <ytsmallpotato@qq.com>
2018-07-07 23:57:44 +0800

    第一次提交

---
+++ src/main/java/com/yt/bean/Event.java
@@ -0,0 +1,30 @@
+package com.yt.bean;
+
+import java.util.Date;
+
+public class Event {
+	private Long id;
+	private String title;
+	private Date date;
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public Date getDate() {
+		return date;
+	}
+	public void setDate(Date date) {
+		this.date = date;
+	}
+	
+	
+	
+}
